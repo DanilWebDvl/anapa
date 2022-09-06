35ab177670e1d086cb544948675a4cf0b5b2a87a
atupikov <atupikov@itrack.ru>
2022-09-06 11:30:49 +0300

    удалил верстку, оставил натяжку

--- html/pagination.php
+++
@@ -1,8 +0,0 @@
-<div class="pagination flex">
-    <a href="" class="arrow part"><img src="img/ico/arrow.svg" alt=""></a>
-    <a href="" class="num part">1</a>
-    <a href="" class="num part active">2</a>
-    <a href="" class="num part">3</a>
-    <a href="" class="num part">4</a>
-    <a href="" class="arrow part"><img src="img/ico/arrow.svg" alt=""></a>
-</div>
