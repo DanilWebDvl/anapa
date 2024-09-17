66dc00686b9169134616812312e1592413634548
Anton <ForWorkCodes@gmail.com>
2024-09-17 15:38:18 +0100

    правки меню

--- local/templates/dinamo/components/bitrix/menu/top_menu/template.php
+++ local/templates/dinamo/components/bitrix/menu/top_menu/template.php
@@ -13,14 +13,46 @@
 /** @var \CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<? if (!empty($arResult)): ?>
+<?php if (!empty($arResult)): ?>
     <nav>
         <ul class="flex flex-center menu">
-            <? foreach ($arResult as $arItem): if ($arItem['TEXT'] == '') continue; ?>
-                <li class="part<?=($arItem['SELECTED'] == true) ? ' active' : '' ?>">
-                    <a href="<?=$arItem['LINK'] ?>"><?=$arItem['TEXT'] ?></a>
-                </li>
-            <? endforeach; ?>
+            <?php $previousLevel = 0; ?>
+            <?php foreach ($arResult as $arItem): if ($arItem['TEXT'] == '' || $arItem['PARAMS']['HIDE_IN_MEGA'] == 'Y' || $arItem['PARAMS']['UF_HIDE_IN_MEGA']) continue; ?>
+                <?php if ($previousLevel && $arItem["DEPTH_LEVEL"] < $previousLevel):?>
+                    <?=str_repeat("</ul></li>", ($previousLevel - $arItem["DEPTH_LEVEL"]));?>
+                <?php endif?>
+
+                <?php if ($arItem["IS_PARENT"]):?>
+
+                    <?if ($arItem["DEPTH_LEVEL"] == 1):?>
+                        <li class="part<?=($arItem['SELECTED'] == true) ? ' active' : '' ?>">
+                            <a href="<?=$arItem['LINK'] ?>" class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?>"><?=$arItem['TEXT'] ?></a>
+                            <ul class="child_list">
+                    <?php else:?>
+                        <li<?if ($arItem["SELECTED"]):?> class="selected"<?endif?>>
+                            <a href="<?=$arItem["LINK"]?>" class="parent"><?=$arItem["TEXT"]?></a>
+                            <ul class="child_list">
+                    <?php endif?>
+
+                <?php else:?>
+                    <?if ($arItem["DEPTH_LEVEL"] == 1):?>
+                        <li class="part<?=($arItem['SELECTED'] == true) ? ' active' : '' ?>">
+                            <a href="<?=$arItem['LINK'] ?>" class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?>"><?=$arItem['TEXT'] ?></a>
+                        </li>
+                    <?else:?>
+                        <li<?if ($arItem["SELECTED"]):?> class="item-selected"<?endif?>>
+                            <a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
+                        </li>
+                    <?endif?>
+                <?php endif?>
+
+
+                <?php $previousLevel = $arItem["DEPTH_LEVEL"];?>
+            <?php endforeach; ?>
+
+            <?php if ($previousLevel > 1)://close last item tags?>
+                <?=str_repeat("</ul></li>", ($previousLevel-1) );?>
+            <?php endif ?>
         </ul>
     </nav>
-<? endif ?>
+<?php endif ?>
--- local/templates/dinamo/header.php
+++ local/templates/dinamo/header.php
@@ -79,13 +79,13 @@ $is_main_page = ($APPLICATION->GetCurPage() == '/') ? true : false;
                         "ALLOW_MULTI_SELECT" => "N",
                         "CHILD_MENU_TYPE" => "left",
                         "DELAY" => "N",
-                        "MAX_LEVEL" => "1",
+                        "MAX_LEVEL" => "2",
                         "MENU_CACHE_GET_VARS" => array(""),
                         "MENU_CACHE_TIME" => "3600",
                         "MENU_CACHE_TYPE" => "A",
                         "MENU_CACHE_USE_GROUPS" => "Y",
                         "ROOT_MENU_TYPE" => "top",
-                        "USE_EXT" => "N"
+                        "USE_EXT" => "Y"
                     )
                 );?>
             </div>
